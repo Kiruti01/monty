e518c288ff2fe5946702c7711e7a4c444bb07073
johhnmunene <edwardsjohnny64@gmail.com>
2023-08-24 13:56:24 +0300

    Implement the mul opcode.error

--- monty.h
+++ monty.h
@@ -69,7 +69,7 @@ void f_pint(stack_t **head, unsigned int number);
 void f_queue(stack_t **head, unsigned int number);
 void f_stack(stack_t **head, unsigned int number);
 void f_swap(stack_t **head, unsigned int number);
-void f_mul(stack_t **head, unsigned int line_number)
+void f_mul(stack_t **head, unsigned int line_number);
 void f_rotl(stack_t **head, unsigned int number);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int number);
 
