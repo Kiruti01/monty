0b9594a03093f5fcbba3ba82a1d597c6b51cdcd9
johhnmunene <edwardsjohnny64@gmail.com>
2023-08-24 16:53:50 +0300

    Implement the mul opcode.error

--- mul.c
+++ mul.c
@@ -5,22 +5,22 @@
  * @number: line_number
  *Return:nothing
 */
-void f_mul(stack_t **head,unsigned int number)
+void f_mul(stack_t **head, unsigned int number)
 {
-        stack_h *h;
+        stack_t *h;
         int len = 0,aux;
 
         h = *head;
         while(h)
         {
                 h = h->next;
-                len ++;
+                len++;
         }
         if(len < 2)
         {
-                fprintf(stderr,"L%d:can't mul,stack too short\n"number);
-                fclose(bus.file);
-                freebus.content);
+                fprintf(stderr,"L%d:can't mul,stack too short\n", number);      
+		fclose(bus.file);
+                free(bus.content);
                 free_stack(*head);
                 exit(EXIT_FAILURE);
         }
